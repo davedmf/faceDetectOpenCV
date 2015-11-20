cf495da86f95ab23d0e3fa57ac0d828d706558d0
Dave <debashisbanerjee@digitalaptech.com>
2015-11-20 14:59:11 +0530

    Initial Commit

---
+++ OpencvTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpencvTest
+//
+//  Created by Rajiv Ghosh on 20/11/15.
+//  Copyright © 2015 digitalaptech. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
